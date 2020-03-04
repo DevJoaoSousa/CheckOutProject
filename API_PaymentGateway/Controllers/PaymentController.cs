7287e452824291b98f4d95723058f395590255ae
Sousa Joao <joao.sousa@natixis.com>
2020-03-04 17:31:48 +0000

    Add project files.

---
+++ API_PaymentGateway/Controllers/PaymentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_PaymentGateway.Controllers
+{
+    public class PaymentController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetPaymentDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProcessPayment()
+        {
+            return View();
+        }
+    }
+}
---
+++ Domain/Entities/PaymentProcess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class PaymentProcess
+    {
+        public long CardNumber { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+        public string CVV { get; set; }
+    }
+}
